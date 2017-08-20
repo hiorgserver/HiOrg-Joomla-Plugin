40ff8b1b0396927742d62342064cfa4aa345a37b
Markus Jungbluth <markusju@users.noreply.github.com>
2017-08-20 22:03:41 +0200

    Fehler behoben.
    Keine Kompatibilität zu DPCal in neuerer Version.
    Fehlerhafte Anzeige von Orten

--- dpcalendar_hiorg.php
+++ dpcalendar_hiorg.php
@@ -5,59 +5,42 @@
  * @copyright	Copyright (C) 2012 Digital Peak, Inc. All rights reserved.
  * @license		http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
-
 defined('_JEXEC') or die();
-
 JLoader::import('components.com_dpcalendar.libraries.dpcalendar.plugin', JPATH_ADMINISTRATOR);
-if (! class_exists('DPCalendarPlugin')) {
+if (! class_exists('DPCalendar\Plugin\DPCalendarPlugin')) {
     return;
 }
-class plgDPCalendarDPCalendar_Hiorg extends DPCalendarPlugin {
+class plgDPCalendarDPCalendar_Hiorg extends DPCalendar\Plugin\DPCalendarPlugin {
     
     
-        protected $hiorg_url = "https://www.hiorg-server.de/termine.php?ical=1";
+    protected $hiorg_url = "https://www.hiorg-server.de/termine.php?ical=1";
 	protected $identifier = 'h';
         
-        
-        public function __construct(&$subject, $config = array()) {
-            if ($this->isImport()) {
-                JError::raiseError(500, "Das HiOrg-Plugin unterstuezt den Import nicht.");
-                die("Importing is not supported and not needed in order for this plugin to operate correctly!. - Das Importieren wird nicht unterstuetzt und nicht benoetigt, damit dieses Plugin korrekt funktioniert.");
-            }
-            parent::__construct($subject, $config);
+    public function __construct(&$subject, $config = array()) {
+        if ($this->isImport()) {
+            JError::raiseError(500, "Das HiOrg-Plugin unterstuezt den Import nicht.");
+            die("Importing is not supported and not needed in order for this plugin to operate correctly!. - Das Importieren wird nicht unterstuetzt und nicht benoetigt, damit dieses Plugin korrekt funktioniert.");
         }
+        parent::__construct($subject, $config);
+    }
         
-	public function fetchCalendars() {	
-            $calendars = array();
-	    $title = $this->params->get('title-1', null);
-            $calendars[] = $this->createCalendar(1, $title, $this->params->get('description-1', ''), $this->params->get('color-1', 'A32929'));
-            
-            //var_dump($calendars);
-            return $calendars;
+    public function fetchCalendars($calendarIds = NULL) {
+        $calendars = array();
+        $title = $this->params->get('title-1', null);
+        $calendars[] = $this->createCalendar(1, $title, $this->params->get('description-1', ''), $this->params->get('color-1', 'A32929'));
+        
+        return $calendars;
                 
-	}
+    }
         
         
-        
-       protected function getContent() {
+    protected function getContent($calendarId, JDate $startDate, JDate $endDate, Joomla\Registry\Registry $options) {
         $uri = $this->hiorg_url."&ov=".$this->params->get('ov-1', null);
         $content = DPCalendarHelper::fetchContent(str_replace('webcal://', 'https://', $uri));
-
         $content = str_replace("BEGIN:VCALENDAR\r\n", '', $content);
         $content = str_replace("\r\nEND:VCALENDAR", '', $content);
-        $this->str_replace_broken_html("\\n", "<br>", $content);
-        //echo $uri;
-        //var_dump($content);
-        /*
-        foreach ($arr as $key => $val) {
-            echo $key;
-            $this->str_replace_broken_html($val, $key, $content);
-        }
-        $this->str_replace_broken_html("\\n", "<br>", $content);
-        */
         return "BEGIN:VCALENDAR\r\n" . $content . "\r\nEND:VCALENDAR";
     }
-
     
     private function isImport() {
         
@@ -72,32 +55,9 @@ class plgDPCalendarDPCalendar_Hiorg extends DPCalendarPlugin {
             }
         }
         }
-		return JFactory::getApplication()->isAdmin() && JRequest::getVar('option', null) == 'com_dpcalendar' &&
-		(JRequest::getVar('task', null) == 'add' && $bool /*|| (JRequest::getVar('view', null) == 'tools' && JRequest::getVar('layout', null) == 'import')*/);
-	}
-        
-        /**
-         * Findet Vorkommen von $needle in $haystack und ersetzt diese druch $repl, auch wenn $needle durch Zeilenumbrüche getrennt ist.
-         * 
-         * @param type $needle
-         * @param type $repl
-         * @param type $haystack
-         */
-        private function str_replace_broken_html($needle, $repl, &$haystack) {
-    
-        $haystack = str_replace($needle, $repl, $haystack);
-        for ($index = 1; $index < strlen($needle); $index++) {
-        $temp=substr($needle, 0, $index);
-        $temp=$temp."\r\n  ";
-        $temp2 = substr($needle, $index);
-        $temp = $temp.$temp2;
-        $haystack = str_replace($temp, $repl, $haystack);
+        return JFactory::getApplication()->isAdmin() && JRequest::getVar('option', null) == 'com_dpcalendar' &&
+        (JRequest::getVar('task', null) == 'add' && $bool /*|| (JRequest::getVar('view', null) == 'tools' && JRequest::getVar('layout', null) == 'import')*/);
+    }
    
-         }
-    
-    
-    
-}
-
     
 }
